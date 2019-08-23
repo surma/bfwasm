a3c33cdf9c62c2b06e0a74c440e2c39f3d7855ee
Surma <surma@surma.link>
2019-08-24 00:30:21 +0100

    First partial compiler

---
+++ compiler.js
@@ -0,0 +1,180 @@
+function* leb128(v) {
+  while (v > 127) {
+    yield (1 << 7) | (v & 0xff);
+    v = Math.floor(v >> 7);
+  }
+  yield v;
+}
+
+function compileBrainfuck(bf) {
+  const code = [...bf].flatMap(c => {
+    switch (c) {
+      case "+":
+        return [
+          ...[
+            // global.get 0
+            ...leb128(0x23),
+            ...leb128(0)
+          ],
+          ...[
+            // global.get 0
+            ...leb128(0x23),
+            ...leb128(0)
+          ],
+          ...[
+            // i32.load
+            ...leb128(0x28),
+            ...leb128(0), // alignment
+            ...leb128(0) // offset
+          ],
+          ...[
+            // i32.const 1
+            ...leb128(0x41),
+            ...leb128(1)
+          ],
+          ...[
+            // i32.add
+            ...leb128(0x6a)
+          ],
+          ...[
+            // i32.store
+            ...leb128(0x36),
+            ...leb128(0), // alignment
+            ...leb128(0) // offset
+          ]
+        ];
+      case ">":
+        return [
+          ...[
+            // global.get 0
+            ...leb128(0x23),
+            ...leb128(0)
+          ],
+          ...[
+            // i32.const 4
+            ...leb128(0x41),
+            ...leb128(4)
+          ],
+          ...[
+            // i32.add
+            ...leb128(0x6a)
+          ],
+          ...[
+            // global.set 0
+            ...leb128(0x24),
+            ...leb128(0)
+          ]
+        ];
+    }
+  });
+
+  return new Uint8Array([
+    ...Buffer.from("\0asm"), // Magic
+    ...[1, 0, 0, 0], // Version
+    ...leb128(1), // Type section
+    ...leb128(4), // Length
+    ...[
+      // Vector of func types
+      ...leb128(1), // Length
+
+      ...[
+        ...leb128(0x60), // Func type
+        ...[
+          // Vector of paramters
+          ...leb128(0) // Length
+        ],
+        ...[
+          // Vector of return types
+          ...leb128(0) // Length
+        ]
+      ]
+    ],
+    ...leb128(3), // Function section
+    ...leb128(2), // Length
+    ...[
+      // Vector of function types
+      ...leb128(1), // Length
+      ...leb128(0) // Function 0 has type 0
+    ],
+    ...leb128(5), // Memory section
+    ...leb128(3), // Length
+    ...[
+      // Vector of memories
+      ...leb128(1), // Length
+      ...[
+        // Memory type
+        ...leb128(0), // Minimum only
+        ...leb128(1) // Number of pages page
+      ]
+    ],
+    ...leb128(6), // Global section
+    ...leb128(6),
+    ...[
+      // Vector of globals
+      ...leb128(1), // Length
+      ...[
+        // Global type
+        ...leb128(0x7f), // i32
+        ...leb128(0x01), // Mutable
+        ...[
+          // Expr
+          ...leb128(0x41), // i32.const
+          ...leb128(0), // Value
+          ...leb128(0x0b) // End
+        ]
+      ]
+    ],
+    ...leb128(7), // Export section
+    ...leb128(10), // Length
+    ...[
+      // Vector of exports
+      ...leb128(1), // Length
+      ...[
+        // Export
+        ...[
+          // Vector of bytes
+          ...leb128(6),
+          ...Buffer.from("memory")
+        ],
+        ...leb128(0x02), // Memory
+        ...leb128(0) // Index 0
+      ]
+    ],
+    ...leb128(8), // Start section
+    ...leb128(1), // Length
+    ...[
+      ...leb128(0) // Function 0
+    ],
+    ...leb128(10), // Code section
+    ...leb128(4 + code.length), // Length
+    ...[
+      // Vector of function bodies
+      ...leb128(1), // Length
+      ...[
+        // Body
+        ...leb128(2 + code.length), // Size in bytes
+        ...[
+          // Code
+          ...[
+            // Vector of locals
+            ...leb128(0) // Length
+          ],
+          ...[
+            // Instructions
+            ...code,
+            ...leb128(0x0b) // End
+          ]
+        ]
+      ]
+    ]
+  ]).buffer;
+}
+
+async function init() {
+  const wasm = compileBrainfuck("+++>++>+");
+  require("fs").writeFileSync("output.wasm", Buffer.from(wasm));
+
+  const { instance } = await WebAssembly.instantiate(wasm);
+  console.log(new Uint32Array(instance.exports.memory.buffer, 0, 10));
+}
+init();
