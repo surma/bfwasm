728f114eaed2f80a5c92c72d8a9a908930f08222
Surma <surma@surma.link>
2019-08-24 15:13:37 +0100

    Added a JIT thingy

---
+++ jit.mjs
@@ -0,0 +1,43 @@
+import { promises as fsp } from "fs";
+import { compile } from "./compiler.mjs";
+import commander from "commander";
+
+const program = new commander.Command();
+program
+  .option("--outfile <file>", "File to write compiled Wasm to")
+  .option("--no-run", "Don’t run compiled Wasm")
+  .option("--mem-dump <N>", "Dump the first N cells of memory after run")
+  .parse(process.argv);
+
+const importObj = {
+  env: {
+    in() {
+      return 0;
+    },
+    out(v) {
+      process.stdout.write(Buffer.from([v]));
+    }
+  }
+};
+
+(async function run() {
+  if (program.args.length !== 1) {
+    program.outputHelp();
+    process.exit(1);
+  }
+  const input = await fsp.readFile(program.args[0], "utf8");
+  const wasm = compile(input);
+  if (program.outfile) {
+    await fsp.writeFile(program.outfile, Buffer.from(wasm));
+  }
+  if (program.run) {
+    const { instance } = await WebAssembly.instantiate(wasm, importObj);
+    if (program.memDump) {
+      console.log(
+        [...new Uint32Array(instance.exports.memory.buffer, 0, program.memDump)]
+          .map(v => v.toString(16).padStart(8, "0"))
+          .join(" ")
+      );
+    }
+  }
+})();
